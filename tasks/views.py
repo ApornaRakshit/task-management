cd94fa17ff2d251dc537964b254d93e55cfe339f
Aporna16 <rakshit.aporna@gmail.com>
2025-01-20 02:52:36 +0600

    Dashboard's user, manager part added

--- tasks/views.py
+++ tasks/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Welcome to the task management system")
 
-def contact(request):
-    return HttpResponse("<h1 style='color:red'>This is contact page</h1>")
+def manager_dashboard(request):
+    return render(request, "dashboard/manager-dashboard.html")
 
-def show_task(request):
-    return HttpResponse("This is our task page")
+
+def user_dashboard(request):
+    return render(request, "dashboard/user-dashboard.html")
